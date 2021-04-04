28d25ee5fce732cbd0b7d922e862f8457f4d373c
LeilaAfhim <leilaafhim@gmail.com>
2021-04-04 11:20:45 +0200

    Painting id created

--- src/Painting.js
+++ src/Painting.js
@@ -7,8 +7,22 @@ import Chat from"./images/chat.svg";
 import Picture from "./images/painting.jpg";
 import Check from "./images/check.svg";
 import Filter from "./images/filter.svg";
-import Dropdown from 'react-bootstrap/Dropdown'
-import DropdownButton from 'react-bootstrap/DropdownButton'
+import Map from "./images/map.PNG";
+import Doc from "./images/doc.svg";
+import Lana from "./images/lana.png";
+import Rob from "./images/Rob.png";
+import Lucas from "./images/Lucas.png";
+import Alice from "./images/Alice.png";
+import English from "./images/en.svg";
+import Esp from "./images/es.svg";
+import Italian from "./images/it.svg";
+import French from "./images/fr.svg";
+
+
+import Dropdown from 'react-bootstrap/Dropdown';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 
@@ -24,7 +38,9 @@ return(
 
 <div className="optionTasksPainting">
    <Avatar className="Filter" name="Filter" src={Filter}size="30px"/> 
- 
+ <input className="SearchInput"
+        type="text"
+        placeholder="Search"/>
    <Dropdown className="Dropdown">
   <Dropdown.Toggle className="DropName" variant="success" id="dropdown-basic">
     MY FILTER
@@ -38,6 +54,8 @@ return(
     </button>
   </Dropdown.Menu>
 </Dropdown>
+
+
 </div>
 
 <div>
@@ -134,16 +152,275 @@ return(
 <button className="MyTasks">Assign the task(s)</button>
 </button>
 
-
-
-
-
 </Scrollbar>
 </div>
 
 
 <div>
   <Avatar className="Chat" name="Chat"  src= {Chat} size="40px"/>  
+
+
+
+////////
+
+
+
+<h1 className="TitlePaintingID">PAINTING ID</h1>
+
+<div className="optionTasksPainting">
+   <Avatar className="Filter" name="Filter" src={Filter}size="30px"/> 
+ <input className="SearchInput"
+        type="text"
+        placeholder="Search"/>
+   <Dropdown className="Dropdown">
+  <Dropdown.Toggle className="DropName" variant="success" id="dropdown-basic">
+    MY FILTER
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu >
+      <button className="MyFilterSaved">
+    <Dropdown.Item href="#/action-1">YC-REL1</Dropdown.Item><br/>
+    <Dropdown.Item href="#/action-2">PAS-POR</Dropdown.Item><br/>
+   
+    </button>
+  </Dropdown.Menu>
+</Dropdown>
+</div>
+
+<div>
+<Scrollbar className="scrollbarPaintingID" style={{ width: 1370, height: 670 }}>
+<button className="PaintingCardID">
+<h5></h5><button className="Edit">EDIT</button>
+
+<Container>
+    <Row>
+         <Col sm={6}>
+<button className="PictureCard">
+   <p className="Title">RA135468</p>
+    <img className="Picture" src={Picture} width="200px"/>
+    <p className="Text">
+  Lorem Ipsum<br/>      
++32 000 00 00<br/>
+dd/mm/yyyy<br/>
+Expressionist<br/>
+Abstract<br/>
+</p>
+</button>
+</Col>
+<Col sm={3}>
+    <button className="MapCard">
+        <p className="Title">MAP</p>
+        <img className="PictureMap" src={Map} width="200px"/>
+        <p className="Text">1th floor - 16B</p>
+    </button>
+</Col>
+</Row>
+
+<Row>
+
+ <Col sm={6}>
+<button className="DescriptionCard">
+    <p className="Title">DESCRIPTION</p>
+   <p className="Text">Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+</button>
+</Col>
+
+<Col sm={3}>
+<button className="ScriptCard">
+ <p className="Title">SCRIPT TOUR</p>   
+ <button className="ButtonScript">
+ <p className="Text">Young Child</p>
+ <button className="ButtonDoc"><Avatar  name="Alice" src={Doc}size="20px"/> </button>
+ <Avatar name="Alice" src={Alice}size="35px" round="20px"/> 
+ <p>03.12.20</p>
+</button>
+<button className="ButtonScript">
+<p className="Text">Teenager</p>
+ <button className="ButtonDoc"><Avatar  name="Alice" src={Doc}size="20px"/> </button>
+ <Avatar name="Alice" src={Lana}size="35px" round="20px"/> 
+ <p>03.12.20</p>
+ </button>
+
+ <button className="ButtonScript">
+<p className="Text">Passionate</p>
+ <button className="ButtonDoc"><Avatar  name="Alice" src={Doc}size="20px"/> </button>
+ <Avatar name="Alice" src={Rob}size="35px" round="20px"/> 
+ <p>03.12.20</p>
+ </button>
+
+ <button className="ButtonScript">
+<p className="Text">Blind Person</p>
+ <button className="ButtonDoc"><Avatar  name="Alice" src={Doc}size="20px"/> </button>
+ <Avatar name="Alice" src={Lucas}size="35px" round="20px"/> 
+ <p>03.12.20</p>
+ </button> 
+
+</button>
+</Col>
+<Col sm={3}>
+<button className="VoiceCard">
+<p className="Title">VOICE TOUR</p>
+ <button className="ButtonVoice">
+<p className="Text">Young Child</p>
+<button className="ButtonDoc"><Avatar  name="Eng" src={English}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="Esp" src={Esp}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="fr" src={French}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="it" src={Italian}size="30px"/> </button>
+</button>
+
+<button className="ButtonVoice">
+<p className="Text">Teenager</p>
+<button className="ButtonDoc"><Avatar  name="Eng" src={English}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="Esp" src={Esp}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="fr" src={French}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="it" src={Italian}size="30px"/> </button>
+</button>
+
+<button className="ButtonVoice">
+<p className="Text">Passionate</p>
+<button className="ButtonDoc"><Avatar  name="Eng" src={English}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="Esp" src={Esp}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="fr" src={French}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="it" src={Italian}size="30px"/> </button>
+</button>
+
+<button className="ButtonVoice">
+<p className="Text">Blind Person</p>
+<button className="ButtonDoc"><Avatar  name="Eng" src={English}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="Esp" src={Esp}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="fr" src={French}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="it" src={Italian}size="30px"/> </button>
+</button>
+
+</button>
+</Col>
+
+</Row>
+
+</Container>
+</button>
+
+
+
+<button className="PaintingCardID">
+<h5></h5><button className="Edit">EDIT</button>
+
+<Container>
+    <Row>
+         <Col sm={6}>
+<button className="PictureCard">
+   <p className="Title">RA135468</p>
+    <img className="Picture" src={Picture} width="200px"/>
+    <p className="Text">
+  Lorem Ipsum<br/>      
++32 000 00 00<br/>
+dd/mm/yyyy<br/>
+Expressionist<br/>
+Abstract<br/>
+</p>
+</button>
+</Col>
+<Col sm={3}>
+    <button className="MapCard">
+        <p className="Title">MAP</p>
+        <img className="PictureMap" src={Map} width="200px"/>
+        <p className="Text">1th floor - 16B</p>
+    </button>
+</Col>
+</Row>
+
+<Row>
+
+ <Col sm={6}>
+<button className="DescriptionCard">
+    <p className="Title">DESCRIPTION</p>
+   <p className="Text">Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+</button>
+</Col>
+
+<Col sm={3}>
+<button className="ScriptCard">
+ <p className="Title">SCRIPT TOUR</p>   
+ <button className="ButtonScript">
+ <p className="Text">Young Child</p>
+ <button className="ButtonDoc"><Avatar  name="Alice" src={Doc}size="20px"/> </button>
+ <Avatar name="Alice" src={Alice}size="35px" round="20px"/> 
+ <p>03.12.20</p>
+</button>
+<button className="ButtonScript">
+<p className="Text">Teenager</p>
+ <button className="ButtonDoc"><Avatar  name="Alice" src={Doc}size="20px"/> </button>
+ <Avatar name="Alice" src={Lana}size="35px" round="20px"/> 
+ <p>03.12.20</p>
+ </button>
+
+ <button className="ButtonScript">
+<p className="Text">Passionate</p>
+ <button className="ButtonDoc"><Avatar  name="Alice" src={Doc}size="20px"/> </button>
+ <Avatar name="Alice" src={Rob}size="35px" round="20px"/> 
+ <p>03.12.20</p>
+ </button>
+
+ <button className="ButtonScript">
+<p className="Text">Blind Person</p>
+ <button className="ButtonDoc"><Avatar  name="Alice" src={Doc}size="20px"/> </button>
+ <Avatar name="Alice" src={Lucas}size="35px" round="20px"/> 
+ <p>03.12.20</p>
+ </button> 
+
+</button>
+</Col>
+<Col sm={3}>
+<button className="VoiceCard">
+<p className="Title">VOICE TOUR</p>
+ <button className="ButtonVoice">
+<p className="Text">Young Child</p>
+<button className="ButtonDoc"><Avatar  name="Eng" src={English}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="Esp" src={Esp}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="fr" src={French}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="it" src={Italian}size="30px"/> </button>
+</button>
+
+<button className="ButtonVoice">
+<p className="Text">Teenager</p>
+<button className="ButtonDoc"><Avatar  name="Eng" src={English}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="Esp" src={Esp}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="fr" src={French}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="it" src={Italian}size="30px"/> </button>
+</button>
+
+<button className="ButtonVoice">
+<p className="Text">Passionate</p>
+<button className="ButtonDoc"><Avatar  name="Eng" src={English}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="Esp" src={Esp}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="fr" src={French}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="it" src={Italian}size="30px"/> </button>
+</button>
+
+<button className="ButtonVoice">
+<p className="Text">Blind Person</p>
+<button className="ButtonDoc"><Avatar  name="Eng" src={English}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="Esp" src={Esp}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="fr" src={French}size="30px"/> </button>
+<button className="ButtonDoc"><Avatar  name="it" src={Italian}size="30px"/> </button>
+</button>
+
+</button>
+</Col>
+
+</Row>
+
+</Container>
+
+</button>
+</Scrollbar>
+</div>
+
+
+
+
+
+
 </div>
 </div>
 );
--- src/Statistics.js
+++ src/Statistics.js
@@ -1,11 +1,99 @@
 import React from "react";
+import { Scrollbar } from "react-scrollbars-custom";
+import Avatar from 'react-avatar';
+import Member from "./Member";
+import './Statistics.css';
+import Chat from"./images/chat.svg";
+import Picture from "./images/painting.jpg";
+import Check from "./images/check.svg";
+import Filter from "./images/filter.svg";
+import Map from "./images/map.PNG";
+import Doc from "./images/doc.svg";
+import Lana from "./images/lana.png";
+import Rob from "./images/Rob.png";
+import Lucas from "./images/Lucas.png";
+import Alice from "./images/Alice.png";
+import English from "./images/en.svg";
+import Esp from "./images/es.svg";
+import Italian from "./images/it.svg";
+import French from "./images/fr.svg";
+
+
+import Dropdown from 'react-bootstrap/Dropdown';
 
 
 
 export default function Statistics (){
 return(
 <div>
-Statistics
+ <Member/>
+ 
+<h1>STATISTICS</h1>
+
+<div className="optionTasksPainting">
+   <Avatar className="Filter" name="Filter" src={Filter}size="30px"/> 
+ <input className="SearchInput"
+        type="text"
+        placeholder="Search"/>
+   <Dropdown className="Dropdown">
+  <Dropdown.Toggle className="DropName" variant="success" id="dropdown-basic">
+    MY FILTER
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu >
+      <button className="MyFilterSaved">
+    <Dropdown.Item href="#/action-1">YC-REL1</Dropdown.Item><br/>
+    <Dropdown.Item href="#/action-2">PAS-POR</Dropdown.Item><br/>
+   
+    </button>
+  </Dropdown.Menu>
+</Dropdown>
+
+<div>
+    <button className="CardMemberStat">
+        <h3 className="CardTitle">MEMBER</h3>
+
+<div className="optionTasksPainting">
+   <Avatar className="Filter" name="Filter" src={Filter}size="30px"/> 
+ <input className="SearchInput"
+        type="text"
+        placeholder="Search"/>
+   <Dropdown className="Dropdown">
+  <Dropdown.Toggle className="DropName" variant="success" id="dropdown-basic">
+    MY FILTER
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu >
+      <button className="MyFilterSaved">
+    <Dropdown.Item href="#/action-1">YC-REL1</Dropdown.Item><br/>
+    <Dropdown.Item href="#/action-2">PAS-POR</Dropdown.Item><br/>
+   
+    </button>
+  </Dropdown.Menu>
+</Dropdown>
+</div>
+
+        <button className="AvatarButton">
+             <Avatar className="MemberAvatarStat" name="Lana" src={Lana}size="50px" round="25px"/> <br/>
+             <Avatar className="MemberAvatarStat" name="Lucas" src={Lucas}size="50px" round="25px"/> <br/>
+             <Avatar className="MemberAvatarStat" name="Rob" src={Rob}size="50px" round="25px"/> <br/>
+             <Avatar className="MemberAvatarStat" name="Alice" src={Alice}size="50px" round="25px"/> 
+        </button>
+
+
+<div>
+  <Avatar className="MemberAvatarStat" name="Lana" src={Lana}size="80px" round="40px"/> <br/>
+  <h2>LANA<br/>CURATOR</h2><h6>Tasks Overview - March 21</h6>
+</div>
+
+
+
+
+    </button>
+</div>
+
+
+</div>
 
 </div>
 
